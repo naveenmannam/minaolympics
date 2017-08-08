b6c1d8bd058dd53668d86e70ac4f2cc71f63fba5
Naveen Mannam <naveen.mannam@hotmail.com>
2017-08-08 15:38:41 -0400

    Added server.js file with express app home route and listen

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+var app = express();
+var port = proces.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+	res.send("This is the home page!!!");
+});
+
+app.listen(port, () => {
+	console.log(`Server started on http://localhost:${port}`);
+});
